c34625ad1c278ad15af908d075220484864ba5e2
manishishere <gautammanish975@gmail.com>
2026-01-03 20:59:55 +0545

    c

--- gpacalc/settings.py
+++ gpacalc/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=*))846)*4v$i=+0(q*bm&(47^bupq%+3d(tmxwg0@djo32-n8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*.onrender.com','gpa-calculator-0met.onrender.com']
 
