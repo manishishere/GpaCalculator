878392c5e0af0c176a5785f58479c18aaa307c84
manishishere <gautammanish975@gmail.com>
2026-01-03 20:42:58 +0545

    a

--- gpacalc/settings.py
+++ gpacalc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=*))846)*4v$i=+0(q*bm&(47^bupq%+3d(tmxwg0@djo32-n8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*.onrender.com', 'localhost']
 
 
 # Application definition
